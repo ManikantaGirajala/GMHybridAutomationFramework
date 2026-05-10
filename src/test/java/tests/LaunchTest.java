ef815427df89a24bea7ab0a42c1f512cac62db58
Manikanta Satish Kumar Girajala <manikantagirajala3@gmail.com>
2026-05-10 16:57:34 +0530

    Converted Selenium browser launch script into TestNG automation test
    - Replaced Java main() execution with TestNG framework execution
    - Added @Test annotation for automation test recognition
    - Enabled Maven and Jenkins test execution support
    - Configured Selenium test for TestNG execution flow
    - Updated framework structure to support automation test lifecycle

--- src/test/java/tests/LaunchTest.java
+++ src/test/java/tests/LaunchTest.java
@@ -3,11 +3,13 @@ package tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.time.Duration;
+import org.testng.annotations.Test;
 
 public class LaunchTest {
-    public static void main(String[] args) {
+
+    @Test
+    public void launchBrowser() {
+
         WebDriverManager.chromedriver().setup();
 
         WebDriver driver = new ChromeDriver();
